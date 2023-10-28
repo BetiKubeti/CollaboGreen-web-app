82565499c0badbd4a4cf22cd100f618f1bd0af26
Plo4i <midag@abv.bg>
2023-10-29 00:59:06 +0200

    Added firebase

---
+++ firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB-5CRus4vUyr9i_rdwYI6u7AC4LtvUW3M",
+    authDomain: "collabogreen-3060a.firebaseapp.com",
+    projectId: "collabogreen-3060a",
+    storageBucket: "collabogreen-3060a.appspot.com",
+    messagingSenderId: "273902362972",
+    appId: "1:273902362972:web:8f8ac19268fd326ebea199",
+    measurementId: "G-V5EWS035CM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
