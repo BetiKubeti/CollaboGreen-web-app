74fa3ac919e5c736e0b3bfbbb4ea6124efa9725f
Plo4i <midag@abv.bg>
2023-10-29 15:10:19 +0100

    Made nav logo style

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Routes, Route, Navigate } from "react-router-dom";
 import '../src/App.jsx'
+import Nav from "./components/Nav";
 import AboutPage from "./pages/AboutPage";
 import RegistrationPage from './pages/RegistrationPage';
 
@@ -9,6 +10,7 @@ function App() {
 
   return (
     <>
+      <Nav />
       <main>
         <Routes>
           <Route path="/" element={<RegistrationPage />} />
---
+++ src/components/Nav.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import { NavLink } from "react-router-dom";
+
+export default function Nav() {
+    return (
+        <nav>
+            <div className='nav-container'>
+                <div className='logo'>
+                    <NavLink to="/"><span>Collabo</span>Green</NavLink>
+                </div>
+                <div className='contents'>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/about">Discover Business</NavLink>
+                    <NavLink to="/contact">Create Account/Log In</NavLink>
+                </div>
+            </div>
+        </nav>
+    );
+}
