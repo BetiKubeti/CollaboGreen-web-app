eeb1a9ac2e3f164098ba4e0d1cb8bc31e7a7dec5
Plo4i <midag@abv.bg>
2023-10-29 17:06:56 +0100

    Added main and homepage.css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,25 +1,36 @@
 import React, { useState } from 'react';
 
 export default function HomePage() {
-    return (
-        <>
+    const [searchTerm, setSearchTerm] = useState('');
 
+    const handleSearch = () => {
+        // Implement your search logic here, e.g., send a request to the server.
+        // You can use 'searchTerm' to get the user's input.
+        console.log('Searching for:', searchTerm);
+    };
+
+    return (
         <section className="banner" id="banner">
             <div className="banner-container">
                 <div className="banner-content-container">
                     <div className="banner-text">
-                            <p>Discover businesses and <span>collaborate</span>. Read and write reviews. Keep in mind <span>sustainability</span>.</p>
+                        <p>
+                            Discover businesses and <span>collaborate</span>. Read and write reviews.
+                            Keep in mind <span>sustainability</span>.
+                        </p>
                     </div>
                     <div className="banner-search-bar">
-
+                        <input
+                            type="text"
+                            placeholder="Search for a company, category or location..."
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                        />
+                        <button onClick={handleSearch}>Search</button>
                     </div>
                 </div>
-                <div className="image">
-
-                </div>
+                <div className="image"></div>
             </div>
         </section>
-
-        </>
     );
 }
