8814fc60bf13a1a4e2aca85d1e4d0f96e48296e9
Plo4i <midag@abv.bg>
2023-11-01 13:15:14 +0100

    Changes to register form

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -3,7 +3,7 @@ import { getAuth, createUserWithEmailAndPassword, sendEmailVerification } from '
 import { auth, firestore } from '../../firebase';
 import { doc, setDoc, getFirestore, collection, addDoc } from 'firebase/firestore';
 import Modal from 'react-modal';
-import Footer from '../components/Footer'
+import Footer from '../components/Footer';
 
 const categories = ["Category 1", "Category 2", "Category 3"];
 
@@ -82,89 +82,105 @@ const RegistrationForm = () => {
 
         try {
             const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-            sendEmailVerification(userCredential.user);
-            const docRef = await addDoc(collection(firestore, 'companies'), companyData);
-            console.log('Registration and document creation successful. Document ID:', docRef.id);
+            // Email verification is no longer sent here
+
+            // Your other registration code here
+
+            // Show the email confirmation modal
+            setEmailConfirmationModalOpen(true);
         } catch (error) {
             console.error('Registration error:', error);
             // Handle registration error, e.g., display an error message.
         }
     };
 
+    const requestEmailVerification = async () => {
+        try {
+            await sendEmailVerification(auth.currentUser);
+            setEmailConfirmationModalOpen(true);
+        } catch (error) {
+            console.error('Email verification request error:', error);
+            // Handle the error, e.g., display an error message.
+        }
+    };
+
     return (
-        <><section className='registration-page'>
-            <div className='registration-page-wrap'>
-                <div className='registration-page-container'>
-                    <h2>Create a free account</h2>
-                    <form onSubmit={handleRegistration}>
-                        <p>Company Information:</p>
-                        <div className='input-container'>
-                            <input type="text" placeholder='Company Name' value={companyName} onChange={(e) => setCompanyName(e.target.value)} />
+        <>
+            <section className='registration-page'>
+                <div className='registration-page-wrap'>
+                    <div className='registration-page-container'>
+                        <h2>Create a free account</h2>
+                        <form onSubmit={handleRegistration}>
+                            <p>Company Information:</p>
+                            <div className='input-container'>
+                                <input type="text" placeholder='Company Name' value={companyName} onChange={(e) => setCompanyName(e.target.value)} />
+                            </div>
+                            <div className='error-message'>
+                                {companyNameError && <p>{companyNameError}</p>}
+                            </div>
+                            <div className='input-container'>
+                                <input type="url" placeholder='Website URL: https://example.com' value={websiteURL} onChange={(e) => setWebsiteURL(e.target.value)} />
+                            </div>
+                            <div className='error-message'>
+                                {websiteURLError && <p>{websiteURLError}</p>}
+                            </div>
+                            <div className='input-container'>
+                                <select value={category} onChange={(e) => setCategory(e.target.value)}>
+                                    <option value="" disabled defaultValue>Select a category</option>
+                                    {categories.map((cat) => (
+                                        <option key={cat} value={cat}>
+                                            {cat}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                            <div className='error-message'>
+                                {categoryError && <p>{categoryError}</p>}
+                            </div>
+                            <div className='input-container'>
+                                <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                            </div>
+                            <div className='error-message'>
+                                {emailError && <p>{emailError}</p>}
+                            </div>
+                            <p>Password:</p>
+                            <div className='input-container'>
+                                <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                            </div>
+                            <div className='error-message'>
+                                {passwordError && <p>{passwordError}</p>}
+                            </div>
+                            <div className='input-container'>
+                                <input type="password" placeholder='Confirm Password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                            </div>
+                            <div className='error-message'>
+                                {confirmPasswordError && <p>{confirmPasswordError}</p>}
+                            </div>
+                            <div className='input-container'>
+                                <button type="submit">Sign Up</button>
+                            </div>
+                        </form>
+                        <div className='change-option'>
+                            <p>You already have a CollaboGreen Account?</p>
+                            <a href="/login">Log In here!</a>
                         </div>
-                        <div className='error-message'>
-                            {companyNameError && <p>{companyNameError}</p>}
-                        </div>
-                        <div className='input-container'>
-                            <input type="url" placeholder='Website URL: https://example.com' value={websiteURL} onChange={(e) => setWebsiteURL(e.target.value)} />
-                        </div>
-                        <div className='error-message'>
-                            {websiteURLError && <p>{websiteURLError}</p>}
-                        </div>
-                        <div className='input-container'>
-                            <select value={category} onChange={(e) => setCategory(e.target.value)}>
-                                <option value="" disabled defaultValue>Select a category</option>
-                                {categories.map((cat) => (
-                                    <option key={cat} value={cat}>
-                                        {cat}
-                                    </option>
-                                ))}
-                            </select>
-                        </div>
-                        <div className='error-message'>
-                            {categoryError && <p>{categoryError}</p>}
-                        </div>
-                        <div className='input-container'>
-                            <input type="email" placeholder='Email' value={email} onChange={(e) => setEmail(e.target.value)} />
-                        </div>
-                        <div className='error-message'>
-                            {emailError && <p>{emailError}</p>}
-                        </div>
-                        <p>Password:</p>
-                        <div className='input-container'>
-                            <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
-                        </div>
-                        <div className='error-message'>
-                            {passwordError && <p>{passwordError}</p>}
-                        </div>
-                        <div className='input-container'>
-                            <input type="password" placeholder='Confirm Password' value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
-                        </div>
-                        <div className='error-message'>
-                            {confirmPasswordError && <p>{confirmPasswordError}</p>}
-                        </div>
-                        <div className='input-container'>
-                            <button type="submit">Sign Up</button>
-                        </div>
-                    </form>
-                    <div className='change-option'>
-                        <p>You already have a CollaboGreen Account?</p>
-                        <a href="/login">Log In here!</a>
                     </div>
                 </div>
-            </div>
-            <Modal
-                isOpen={isEmailConfirmationModalOpen}
-                contentLabel="Email Confirmation Modal"
-            >
-                <h2>Check Your Email</h2>
-                <p>An email has been sent to you for confirmation. Please verify your email to complete the registration.</p>
-                <button onClick={() => setEmailConfirmationModalOpen(false)}>Close</button>
-            </Modal>
-        </section>
-        
+        </section >
+
+        <Modal
+            isOpen={isEmailConfirmationModalOpen}
+            contentLabel="Email Confirmation Modal"
+        >
+            <h2>Check Your Email</h2>
+            <p>An email has been sent to you for confirmation. Please verify your email to complete the registration.</p>
+            <button onClick={() => setEmailConfirmationModalOpen(false)}>Close</button>
+            <button type="button" onClick={requestEmailVerification}>Resend Confirmation Email</button>
+        </Modal>
+
         <Footer />
-        </>
-    );
+    </>
+ );
 };
 
 export default RegistrationForm;
