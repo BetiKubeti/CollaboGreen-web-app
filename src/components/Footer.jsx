701c4b6f92c3a95c5d0d77e9babbc7f3fcd6493b
Plo4i <midag@abv.bg>
2023-11-01 11:36:19 +0100

    Added Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'; // Import React module
+
+export default function Footer() {
+    // This is the functional component for the navigation bar
+
+    return (
+        <footer>
+            <div className='footer-container'>
+                <p>&copy; 2023 CollaboGreen All Rights Reserved </p>
+            </div>
+        </footer>
+    );
+}
