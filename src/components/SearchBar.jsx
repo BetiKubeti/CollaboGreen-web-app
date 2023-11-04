94aa7100482c48281aa721ea98ac49dadba11e76
Plo4i <midag@abv.bg>
2023-11-04 16:02:56 +0100

    SeachBar sugestions added

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,36 +1,81 @@
-import React, { useState } from 'react'; // Import React module
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' // Import FontAwesomeIcon component
+import React, { useState, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { firestore } from '../../firebase'; // Import firestore from your 'firebase.js' file
+import { collection, getDocs } from 'firebase/firestore';
 
 export default function SearchBar() {
-    // This is the functional component for the SearchBar
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const [showSuggestions, setShowSuggestions] = useState(false); // State to control visibility of suggestions
 
-    const [searchTerm, setSearchTerm] = useState(''); // Initialize a state variable to store user input
+    useEffect(() => {
+        if (searchTerm) {
+            const query = searchTerm.toLowerCase();
+            const results = [];
+
+            // Use the 'firestore' instance from your 'firebase.js' file
+            getDocs(collection(firestore, 'companies'))
+                .then((querySnapshot) => {
+                    querySnapshot.forEach((doc) => {
+                        const data = doc.data();
+                        if (data && data.companyName && data.category) {
+                            // Check if the company name or category matches the query
+                            if (
+                                data.companyName.toLowerCase().includes(query) ||
+                                data.category.toLowerCase().includes(query)
+                            ) {
+                                results.push(data.companyName);
+                            }
+                        }
+                    });
+
+                    setSearchResults(results);
+                    setShowSuggestions(true); // Show suggestions when there are search results
+                })
+                .catch((error) => {
+                    console.error('Error fetching companies:', error);
+                });
+        } else {
+            setSearchResults([]); // Clear the recommendations when the search term is empty
+            setShowSuggestions(false); // Hide suggestions when there are no search results
+        }
+    }, [searchTerm]);
 
     const handleSearch = () => {
-        // Function to handle the search action
-        // You can implement your search logic here, e.g., sending a request to the server
-        // The 'searchTerm' state variable holds the user's input
-        console.log('Searching for:', searchTerm); // Log the search term to the console
+        console.log('Searching for:', searchTerm);
+        // Implement your search logic here
     };
 
     return (
         <div className="search-bar-container">
-            {/* Container for the entire search bar */}
-            <form className="search-bar" action="">
-                {/* Form element with 'search-bar' class */}
+            <form className="search-bar `search-bar-container ${showSearchResults ? 'active' : ''}`" action="">
                 <input
                     type="text"
                     placeholder="Search for a company, category, or location..."
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
+                    className={showSuggestions ? 'active' : ''}
                 />
-                {/* Input field for user input */}
                 <button onClick={handleSearch}>
-                    {/* Button that triggers the search action */}
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g id="feSearch0" fill="none" fill-rule="evenodd" stroke="none" stroke-width="1"><g id="feSearch1" fill="currentColor"><path id="feSearch2" d="m16.325 14.899l5.38 5.38a1.008 1.008 0 0 1-1.427 1.426l-5.38-5.38a8 8 0 1 1 1.426-1.426ZM10 16a6 6 0 1 0 0-12a6 6 0 0 0 0 12Z"/></g></g></svg>
-                    {/* Include a magnifying glass icon */}
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <g id="feSearch0" fill="none" fill-rule="evenodd" stroke="none" stroke-width="1">
+                            <g id="feSearch1" fill="currentColor">
+                                <path
+                                    id="feSearch2"
+                                    d="m16.325 14.899l5.38 5.38a1.008 1.008 0 0 1-1.427 1.426l-5.38-5.38a8 8 0 1 1 1.426-1.426ZM10 16a6 6 0 1 0 0-12a6 6 0 0 0 0 12Z"
+                                />
+                            </g>
+                        </g>
+                    </svg>
                 </button>
             </form>
+            {showSuggestions && searchResults.length > 0 && (
+                <ul className="search-results">
+                    {searchResults.map((result, index) => (
+                        <li key={index}>{result}</li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 }
