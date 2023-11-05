494fe861949d505ebd46e771d31ce381a7a73a2a
Plo4i <midag@abv.bg>
2023-11-05 14:58:26 +0100

    Finishing work on filters

--- src/pages/CompaniesCategoryPage.jsx
+++ src/pages/CompaniesCategoryPage.jsx
@@ -15,6 +15,9 @@ export default function CompaniesCategoryPage() {
     const [companies, setCompanies] = useState([]);
     const [fetched, setFetched] = useState(false);
     const { category } = useParams();
+    const [activeRating, setActiveRating] = useState('All'); // State for active rating filter
+    const [selectedLocation, setSelectedLocation] = useState(''); // State for selected location
+    const locations = ["Denmark", "Germany", "USA", "Poland"]
 
     console.log('Searching for:', category);
 
@@ -26,7 +29,7 @@ export default function CompaniesCategoryPage() {
                 const companyData = [];
                 querySnapshot.forEach((doc) => {
                     const data = doc.data();
-                    if (data.category === category) {
+                    if (data.category === category && (activeRating === 'All' || data.rating === parseInt(activeRating))) {
                         companyData.push(data);
                     }
                 });
@@ -40,7 +43,12 @@ export default function CompaniesCategoryPage() {
         if (category !== '') {
             fetchCompanies();
         }
-    }, [category]);
+    }, [category, activeRating]);
+
+    // Function to handle rating filter button click
+    const handleRatingFilterClick = (rating) => {
+        setActiveRating(rating);
+    };
 
     return (
         <>
@@ -60,60 +68,107 @@ export default function CompaniesCategoryPage() {
             </section>
 
             <section className='category-lists'>
-                <div className='title'>
-                    <h2>Companies ({companies.length})</h2>
+                
+                <div className='filters-container'>
+                    <div className='filter-by-rating filter'>
+                        <h3>Rating</h3>
+                        <div className='rating-filter'>
+                            <button
+                                className={activeRating === 'All' ? 'active-rating-button' : ''}
+                                onClick={() => handleRatingFilterClick('All')}
+                            >
+                                Any <svg xmlns="http://www.w3.org/2000/svg" className='green-star' width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m5.825 22l1.625-7.025L2 10.25l7.2-.625L12 3l2.8 6.625l7.2.625l-5.45 4.725L18.175 22L12 18.275L5.825 22Z" /></svg>
+                            </button>
+                            {[1, 2, 3, 4, 5].map((rating) => (
+                                <button
+                                    key={rating}
+                                    className={activeRating === rating.toString() ? 'active-rating-button' : ''}
+                                    onClick={() => handleRatingFilterClick(rating.toString())}
+                                >
+                                    {<svg xmlns="http://www.w3.org/2000/svg" className='green-star' width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m5.825 22l1.625-7.025L2 10.25l7.2-.625L12 3l2.8 6.625l7.2.625l-5.45 4.725L18.175 22L12 18.275L5.825 22Z" /></svg>}
+                                    {rating}
+                                </button>
+                            ))}
+                        </div>
+                    </div>
+                    <div className='filter-by-location filter'>
+                        <h3>Location</h3>
+                        <div className='locations-filter'>
+                            <select value={selectedLocation} onChange={(e) => setSelectedLocation(e.target.value)}>
+                                <option value="" disabled hidden selected>Select a location</option>
+                                {locations.map((location) => (
+                                    <option key={location} value={location}>
+                                        {location}
+                                    </option>
+                                ))}
+                            </select>
+                        </div>
+                    </div>
                 </div>
 
-                <div className='company-by-category-container'>
-                    {companies.length === 0 ? (
-                        <div className='no-matches'>
-                            <p>No matches found for {category}</p>
-                        </div>
-                    ) : (
-                        companies.map((company, index) => (
-                            <div className='company-by-category-card' key={index}>
-                                <div className='company-info-container'>
-                                    <div className='company-logo'>
-                                        <img src={LogoExample} alt="Company Logo" />
-                                    </div>
-                                    <div className='company-info'>
-                                        <div className='company-name'>
-                                            <h3>{company.companyName}</h3>
-                                        </div>
-                                        <div className='company-rating'>
-                                            <div className='star-rating'>
-
-                                            </div>
-                                            <div className='trust-score'>
-                                                <p>Trust Score: 4.12</p>
-                                            </div>
-                                            <div className='reviews-number'>
-                                                <p>Reviews: 12,102,123</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className='connections'>
-                                    <div className='social-media'>
-                                        <p>See more:</p>
-                                    </div>
-                                    <div className='website'>
-                                        <p>Website: {company.websiteURL}</p>
-                                    </div>
-                                    <div className='more-reviews'>
-                                        <a href="#">Read Reviews</a>
-                                    </div>
-                                </div>
+                <div className='company-list-container'>
+                    <div className='title'>
+                        <h2>Companies ({companies.length})</h2>
+                    </div>
+                    <div className='company-by-category-container'>
+                        {companies.length === 0 ? (
+                            <div className='no-matches'>
+                                <p>No matches found for {category}</p>
                             </div>
-                        ))
-                    )}
+                        ) : (
+                            companies.map((company, index) => (
+                                <div className='company-by-category-card' key={index}>
+                                    <div className='company-info-container'>
+                                        <div className='company-logo'>
+                                            <img src={LogoExample} alt="Company Logo" />
+                                        </div>
+                                        <div className='company-info'>
+                                            <div className='company-name'>
+                                                <h3>{company.companyName}</h3>
+                                            </div>
+                                            <div className='company-rating'>
+                                                <div className='star-rating'>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" className='green-star' width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m5.825 22l1.625-7.025L2 10.25l7.2-.625L12 3l2.8 6.625l7.2.625l-5.45 4.725L18.175 22L12 18.275L5.825 22Z" /></svg>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" className='green-star' width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m5.825 22l1.625-7.025L2 10.25l7.2-.625L12 3l2.8 6.625l7.2.625l-5.45 4.725L18.175 22L12 18.275L5.825 22Z" /></svg>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" className='green-star' width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m5.825 22l1.625-7.025L2 10.25l7.2-.625L12 3l2.8 6.625l7.2.625l-5.45 4.725L18.175 22L12 18.275L5.825 22Z" /></svg>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" className='green-star' width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m5.825 22l1.625-7.025L2 10.25l7.2-.625L12 3l2.8 6.625l7.2.625l-5.45 4.725L18.175 22L12 18.275L5.825 22Z" /></svg>
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m5.825 22l1.625-7.025L2 10.25l7.2-.625L12 3l2.8 6.625l7.2.625l-5.45 4.725L18.175 22L12 18.275L5.825 22Z" /></svg>
+                                                </div>
+                                                <div className='trust-score'>
+                                                    <p>Trust Score: 4.12</p>
+                                                </div>
+                                                <div className='reviews-number'>
+                                                    <p>Reviews: 12,102,123</p>
+                                                </div>
+                                            </div>
+                                            <div className='location'>
+                                                <p>Location: Aarhus, Denmark</p>
+                                            </div>
+                                            <div className='company-type'>
+                                                <p>Type of company: {category}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className='connections'>
+                                        <div className='social-media'>
+                                            <p>See more:</p>
+                                        </div>
+                                        <div className='website'>
+                                            <p>Website: <a href={company.websiteURL} target='_blank'>{company.websiteURL}</a></p>
+                                        </div>
+                                        <div className='more-reviews'>
+                                            <a href="#">Read Reviews</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        )}
+                    </div>
                 </div>
             </section>
 
             {user && <div></div>}
-            {!user && (
-                <CompanyRegisterQuestion />
-            )}
+            {!user && <CompanyRegisterQuestion />}
 
             <Footer />
         </>
